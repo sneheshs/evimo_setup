4819cf7524767c8b10d3a5908716bca261f0cd2a
Levi Burner <leviburner@gmail.com>
2020-09-22 15:54:51 -0400

    Disable parts of vicon package that try to link at runtime to old boost versions

--- initialize_ev_imo_catkin_workspace.sh
+++ initialize_ev_imo_catkin_workspace.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-set -e
+set -xe
 
 mkdir -p catkin_ws/src
 git clone git@github.com:better-flow/evimo.git --branch evimo2 catkin_ws/src/evimo
 git clone git@github.com:KumarRobotics/vicon.git catkin_ws/src/vicon
+rm -rf catkin_ws/src/vicon/vicon_driver \
+       catkin_ws/src/vicon/vicon_odom \
+       catkin_ws/src/vicon/ipc
+cd catkin_ws/src/vicon
+git apply ../../../vicon_cmake_disable.patch
+cd ../../..
+
 git clone git@github.com:uzh-rpg/rpg_dvs_ros.git catkin_ws/src/rpg_dvs_ros
 rm -rf catkin_ws/src/rpg_dvs_ros/davis_ros_driver \
        catkin_ws/src/rpg_dvs_ros/dvs_calibration  \
