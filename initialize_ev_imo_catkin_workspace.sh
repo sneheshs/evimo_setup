d0533016d60a8304a851c47836e85017ddddd0cb
Levi Burner <leviburner@gmail.com>
2020-09-30 18:47:30 -0400

    New project added to upstream that is not needed

--- initialize_ev_imo_catkin_workspace.sh
+++ initialize_ev_imo_catkin_workspace.sh
@@ -17,5 +17,6 @@ rm -rf catkin_ws/src/rpg_dvs_ros/davis_ros_driver \
        catkin_ws/src/rpg_dvs_ros/dvs_calibration_gui \
        catkin_ws/src/rpg_dvs_ros/dvs_file_writer  \
        catkin_ws/src/rpg_dvs_ros/dvs_renderer     \
-       catkin_ws/src/rpg_dvs_ros/dvs_ros_driver
+       catkin_ws/src/rpg_dvs_ros/dvs_ros_driver   \
+       catkin_ws/src/rpg_dvs_ros/dvxplorer_ros_driver
 
